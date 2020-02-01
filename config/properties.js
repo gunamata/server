3f960ef4c7407f761233a99223a2c10a0f01776b
gunamata <gunasekhar@outlook.com>
2020-02-01 11:43:24 -0800

    Modified TonePredictor port number

--- config/properties.js
+++ config/properties.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT : 4000,
     DB : 'mongodb://localhost:27017/finn-ai-db',
-    TONE_PREDICTOR_URL: 'http://localhost:5000/tone'
+    TONE_PREDICTOR_URL: 'http://localhost:8000/tone'
 }
